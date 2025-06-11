584c542eb98a56ff5a59c49ce6894908c6edc3ae
KRISHANCDRI <140616274+KRISHANCDRI@users.noreply.github.com>
2025-06-11 17:11:43 +0530

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -26,6 +26,30 @@ function getProvider() {
     }
     return null;
 }
+function isMobile() {
+    return /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
+}
+
+connectBtn.addEventListener('click', async () => {
+    const provider = getProvider();
+    if (!provider) {
+        if (isMobile()) {
+            walletAddressDiv.innerHTML = `
+                <span class="text-danger">
+                    Mobile pe Phantom Wallet connect karne ke liye:<br>
+                    <a href="https://phantom.app/ul/browse/${window.location.href}" target="_blank">
+                        Open in Phantom App
+                    </a>
+                </span>`;
+        } else {
+            walletAddressDiv.innerHTML = `<span class="text-danger">
+                Phantom Wallet install karo: <a href="https://phantom.app/" target="_blank">phantom.app</a>
+            </span>`;
+        }
+        return;
+    }
+    // baaki code...
+});
 
 
 
